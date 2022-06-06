5c85db6135cbfcfc00cfb8cc191b893a363306ef
Manas Sharma <manaskgp2593@kgpian.iitkgp.ac.in>
2022-06-06 08:27:53 +0530

    Second Commit

--- shop/models.py
+++ shop/models.py
@@ -5,6 +5,15 @@ from django.db import models
 
 class Product(models.Model):
     product_id = models.AutoField
-    product_name = models.CharField(max_length=50)
-    descp = models.CharField(max_length= 300)
+    product_name = models.CharField(max_length=50, default='')
+    descp = models.CharField(max_length= 300, default='')
     pub_date = models.DateField()
+    category = models.CharField(max_length=50, default='')
+    sub_category = models.CharField(max_length=50, default='')
+    price = models.IntegerField(default=0)
+    image = models.ImageField(upload_to= 'shop/images', default='')
+
+
+
+    def __str__(self) -> None:
+        return self.product_name
